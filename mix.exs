16f7067fb74dbcf182160cff3d925ef5ab4741e1
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2026-06-09 22:11:24 +0200

    Relax req requirement

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule ReqLogger.MixProject do
 
   defp deps do
     [
-      {:req, "~> 0.5.0"},
+      {:req, "~> 0.5"},
       {:bypass, "~> 2.1", only: :test},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
